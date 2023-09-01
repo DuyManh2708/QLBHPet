357b4277320e7f006f41b1f562a54c3297f2ade3
Dao Duy Manh <duymanh27082000@gmail.com>
2023-09-01 16:52:01 +0700

    update

--- controllers/render.controller.js
+++ controllers/render.controller.js
@@ -15,7 +15,8 @@ const RenderController = {
 
   signIn: (req, res, next) => {
     try {
-      res.render(Constant.viewVersion + "/auth/SignIn");
+      console.log(Constant.viewVersion);
+      res.render(Constant.viewVersion + "/auth/signin");
     } catch (error) {
       console.log(error);
     }
@@ -23,6 +24,41 @@ const RenderController = {
   contact: (req, res, next) => {
     try {
       res.render(Constant.viewVersion + "/pages/contacts");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  menu: (req, res, next) => {
+    try {
+      res.render(Constant.viewVersion + "/layout/menu");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  menuleft: (req, res, next) => {
+    try {
+      res.render(Constant.viewVersion + "/layout/menuleft");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  homepage: (req, res, next) => {
+    try {
+      res.render(Constant.viewVersion + "/client/homepage");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  dog: (req, res, next) => {
+    try {
+      res.render(Constant.viewVersion + "/client/dog");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  cat: (req, res, next) => {
+    try {
+      res.render(Constant.viewVersion + "/client/cat");
     } catch (error) {
       console.log(error);
     }
--- routes/website.route.js
+++ routes/website.route.js
@@ -1,9 +1,14 @@
-var express = require('express');
-const RenderController = require('../controllers/render.controller');
+var express = require("express");
+const RenderController = require("../controllers/render.controller");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', RenderController.home) ;
-router.get('/signin', RenderController.signIn);
-router.get('/contact', RenderController.contact);
+router.get("/", RenderController.home);
+router.get("/signin", RenderController.signIn);
+router.get("/contact", RenderController.contact);
+router.get("/menu", RenderController.menu);
+router.get("/menuleft", RenderController.menuleft);
+router.get("/homepage", RenderController.homepage);
+router.get("/dog", RenderController.dog);
+router.get("/cat", RenderController.cat);
 module.exports = router;
